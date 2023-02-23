6b843e046d1e27c58f2395b3da444fd3aebe9161
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-23 07:43:03 -0700

    feat: Completed tutorial

--- Program.cs
+++ Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Example 1
+            //int zero = 0;
+
+            //try
+            //{
+            //    int a = 1 / zero;
+            //}
+            //catch (ArithmeticException ex)
+            //{
+            //    Console.WriteLine("Dividing by zero is futile!");
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+            //catch(StackOverflowException)
+            //{
+
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Let's print this anywho!");
+            //}
+
+            //Console.WriteLine("Alright!  Let's move on");
+            #endregion
+
+            methodOne();
+        }
+
+        static void methodOne()
+        {
+            methodTwo();
+        }
+
+        static void methodTwo()
+        {
+            throw new Exception();
         }
     }
 }
